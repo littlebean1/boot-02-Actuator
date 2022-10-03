f537d7617c4955916390d8ef32f861f27a4d0e55
littlebean1 <190948400@qq.com>
2022-10-03 15:27:41 +0800

    push commit 1

--- src/main/java/com/atguigu/boot02/Boot02Application.java
+++ src/main/java/com/atguigu/boot02/Boot02Application.java
@@ -10,6 +10,7 @@ public class Boot02Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Boot02Application.class, args);
+        System.out.println("test");
     }
 
 }
