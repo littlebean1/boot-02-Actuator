ba0d3cad9f33f18f2c2824ea84247e389d5aa717
littlebean1 <190948400@qq.com>
2022-10-03 15:21:10 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/boot02/Boot02Application.java
@@ -0,0 +1,15 @@
+package com.atguigu.boot02;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class Boot02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot02Application.class, args);
+    }
+
+}
